a681a5c4e27426ffc8be3f24f242ceed02d1c167
Leonardo <leonardode.b.c@gmail.com>
2026-02-24 22:06:40 -0300

    criado logo e menus

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
+
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
-        perspiciatis, architecto enim temporibus rerum quo beatae dolores nihil
-        eum quis blanditiis corporis harum quidem consequuntur? Eum a eaque
-        necessitatibus distinctio.
-      </p>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
     </>
   );
 }
