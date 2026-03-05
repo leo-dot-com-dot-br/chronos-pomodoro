69bcdf27049efdb05e458cf8e2fb0529a8c64f7d
Leonardo <leonardode.b.c@gmail.com>
2026-03-04 21:57:46 -0300

    criado pagina Home e template para as outras, componente genericHtml para as páginas que renderizam HTML e models

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,8 @@
+import { Home } from './pages/Home';
+
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
-
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form action='' className='form'>
-          <div className='formRow'>
-            <DefaultInput
-              labelText='Task:'
-              id='meuInput'
-              type='text'
-              placeholder='Digite a tarefa...'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>
-              Nesse ciclo <b>descanse</b> por <b>5 minutos.</b>
-            </p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,33 @@
+import { PlayCircleIcon } from 'lucide-react';
+import { Cycles } from '../Cycles';
+import { DefaultButton } from '../DefaultButton';
+import { DefaultInput } from '../DefaultInput';
+
+export function MainForm() {
+  return (
+    <form action='' className='form'>
+      <div className='formRow'>
+        <DefaultInput
+          labelText='Task:'
+          id='meuInput'
+          type='text'
+          placeholder='Digite a tarefa...'
+        />
+      </div>
+
+      <div className='formRow'>
+        <p>
+          Nesse ciclo <b>descanse</b> por <b>5 minutos.</b>
+        </p>
+      </div>
+
+      <div className='formRow'>
+        <Cycles />
+      </div>
+
+      <div className='formRow'>
+        <DefaultButton icon={<PlayCircleIcon />} />
+      </div>
+    </form>
+  );
+}
